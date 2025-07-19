6f5fa9247bb413e2c81a00c21109205eff6c812f
Carlos Rojas <carlos200.cd@gmail.com>
2025-07-19 09:39:15 -0400

    camb

--- frontend/src/views/Companies.vue
+++ frontend/src/views/Companies.vue
@@ -417,6 +417,7 @@
 <script setup>
 import { ref, computed, onMounted, watch, nextTick } from 'vue'
 import { useToast } from 'vue-toastification'
+import apiService from '../services/api'
 
 // Estado reactivo
 const companies = ref([])
