e6f52bd496e1d587fd9bcafb662341ac940589b1
Carlos Rojas <carlos200.cd@gmail.com>
2025-07-21 22:59:58 -0400

    cambio logo

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -2,12 +2,7 @@
   <aside class="sidebar">
     <div class="sidebar-header">
       <h2 class="logo">
-        <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 2L2 7L12 12L22 7L12 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M2 17L12 22L22 17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M2 12L12 17L22 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        <span>enviGo</span>
+        <img src="../assets/envigoLogo.png" alt="" srcset="">
       </h2>
     </div>
     <nav class="nav-menu">
