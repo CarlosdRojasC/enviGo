285b3321099bd9f761cd28c739b78ca277c2969d
Carlos Rojas <carlos200.cd@gmail.com>
2025-08-18 17:47:05 -0400

    cambio entregas

--- frontend/src/components/ProofOfDelivery.vue
+++ frontend/src/components/ProofOfDelivery.vue
@@ -39,7 +39,7 @@
 
     <!-- Prueba de Entrega -->
     <div class="proof-evidence">
-      <h3>Prueba de Entrega</h3>
+      <h3>Pruebas de Entrega</h3>
       
       <!-- Fotos -->
       <div class="photos-section" v-if="hasPhotos">
